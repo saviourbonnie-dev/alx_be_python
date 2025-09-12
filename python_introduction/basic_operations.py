b3358e1be4be1fe6f74fdc62f8e33e8c5c2700c6
Saviour Bonnie <saviourbonnie@gmail.com>
2025-09-12 11:14:19 +0000

    Added: my first python program

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,18 @@
+# ALX basic arithmetic exercise
+
+number1 = 10
+number2 = 5
+
+# Addition
+sum = number2 + number1
+
+# Subtraction
+difference = number1 - number2
+
+# multiplication
+product = number2 * number1
+
+# Operation from the execercise
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
