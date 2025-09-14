a5b9581ee56faef1bdab745966f8c8dc42d1daf1
Saviour Bonnie <saviourbonnie@gmail.com>
2025-09-14 20:25:22 +0000

    Modified the formula that converts the hours to seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -10,9 +10,9 @@ one_hour_to_minutes = 60 # minutes
 
 # one hour to seconds
 
-hour_to_seconds = one_minutes_to_seconds * one_hour_to_minutes
+# hour_to_seconds = one_minutes_to_seconds * one_hour_to_minutes
 
 #final conversion of 2 hours to seconds
-seconds = hours * hour_to_seconds
+seconds = hours * 3600
 
 print("2 hour(s) is", seconds, "seconds")
