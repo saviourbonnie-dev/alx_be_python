2a624d6b45ba009c26052661c63b5686333596a3
Saviour Bonnie <saviourbonnie@gmail.com>
2025-09-14 21:36:12 +0000

    Corrected the script to the expected script format

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -12,13 +12,17 @@ print("Your monthly savings are", sep'$', monthly_savings)
 projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 print("Projected Savings after one year, with interest, is:", sep'$', projected_savings)
 """
+
 # Personal Finance Calculator
 
+# User Input for Financial Details:
 monthly_income = float(input("Enter your monthly income: "))
-total_monthly_expense = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = monthly_income - total_monthly_expense
+# Calculate Monthly Savings:
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 print(f"Your monthly savings are: ${monthly_savings:.2f}")
 
+# Project Annual Savings:
 projected_savings = monthly_savings * 12 * 1.05
 print(f"Projected Savings after one year, with interest, is: ${projected_savings:.2f}")
