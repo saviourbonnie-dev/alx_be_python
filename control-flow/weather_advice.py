7a04fae21efca1acf25b359e9071496c3bbcc2f8
Saviour Bonnie <saviourbonnie@gmail.com>
2025-09-21 20:06:50 +0000

    Modified the weather script to hard code the predefined weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,13 @@
 # Weather Recommendation Program
 # weather = ["sunny", "rainy", "cold"]
 
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if current_weather == "sunny":
+if weather == "sunny":
 	print("Wear a t-shirt and sunglasses.")
-elif current_weather == "rainy":
+elif weather == "rainy":
 	print("Don't forget your umbrella and a raincoat.")
-elif current_weather == "cold":
+elif weather == "cold":
 	print("Make sure to wear a warm coat and a scarf.")
 else:
 	print("Sorry, I don't have recommendations for this weather.")
